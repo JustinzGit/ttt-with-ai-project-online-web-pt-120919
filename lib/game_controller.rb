69e3b571c8cc99cc2bd8e3ed2ce0e2a8b4c6a0f0
Justin Branch <justinms@outlook.com>
2020-02-04 02:51:36 +0000

    Automatically backed up by Learn

---
+++ lib/game_controller.rb
@@ -0,0 +1,33 @@
+class GameController
+  
+  def start
+    
+    puts "Welcome to Tic-Tac-Toe"
+    print "Select the number of players 0-2: "
+    player_number = gets.strip
+
+    case player_number
+      when "0"
+        zero_player_game
+      when "1"
+        one_player_game
+      when "2" 
+        two_player_game
+      end 
+  end
+  
+  def zero_player_game
+    game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"))
+    game.play
+  end 
+  
+  def one_player_game
+      game = Game.new(Players::Human.new("X"), Players::Computer.new("O"))
+      game.play
+  end 
+    
+  def two_player_game
+      game = Game.new
+      game.play
+  end
+end 
